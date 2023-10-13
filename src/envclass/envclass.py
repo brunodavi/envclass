990fefe77aac6e2a759bb6aa276b4d2fc0d0cd27
brunodavi <brunodaviandrade2000@gmail.com>
2023-10-12 22:38:48 -0300

    feat: correções e alguns recursos adicionados

--- src/envclass/envclass.py
+++ src/envclass/envclass.py
@@ -1,11 +1,18 @@
+import re
 from typing import IO
 
-
-from os import getenv, environ
+from os import environ
 from dotenv import load_dotenv
 
 
 class EnvClass:
+    _strict = True
+    _class_as_prefix = False
+
+    _prefix = None
+    _joiner = '_'
+
+
     def __init__(
             self,
             env_file: str | None = None,
@@ -25,15 +32,11 @@ class EnvClass:
             encoding
         )
 
-        self.__parse_attributes()
+        self._init_attrs()
 
-
-    def __parse_attributes(self):
-        cls = self.__class__
-        proxy_types = vars(cls)
-        attributes = proxy_types['__annotations__']
+    def _init_attrs(self):
+        attributes = self.__annotations__
         attributes_items = attributes.items()
-
 
         for label, attrib in attributes_items:
             self._label = label
@@ -42,8 +45,37 @@ class EnvClass:
             setattr(
                 self,
                 label,
-                self.__read_only_atributte
+                self._read_only_atributte
             )
+
+    def _get_env(self, label: str, default_exists: bool):
+        if not default_exists and self._strict:
+            return environ[label]
+
+        return environ.get(label)
+
+
+    def parse_label(self, label: str):
+        prefix = ''
+        joiner = self._joiner
+        label = label.upper()
+
+        if self._class_as_prefix:
+            cls_name = type(self).__name__
+
+            words = re.findall(r'[A-Z][a-z-0-9]+', cls_name)
+
+            if len(words) > 1:
+                prefix = joiner.join(words).upper()
+            else:
+                prefix = cls_name.upper()
+
+            prefix += joiner
+
+        if self._prefix is not None:
+            prefix = self._prefix + joiner
+
+        return prefix + label
 
     def parse_attrib(
             self,
@@ -51,11 +83,32 @@ class EnvClass:
             attrib: type,
             default=None
         ):
-        breakpoint()
-        return attrib(getenv(label.upper()) or default)
+        default_exists = default is not None
+        upper_label = self.parse_label(label)
+
+        env_value = self._get_env(
+            upper_label,
+            default_exists,
+        )
+
+        if env_value == '':
+            return None
+
+        if attrib is bool:
+            if env_value in 'True true 1'.split():
+                return True
+            elif env_value in 'False false 0'.split():
+                return False
+
+        if env_value is not None:
+            value = attrib(env_value)
+
+            return value
+
+        return default
 
     def get_attribute(self):
-        default = getattr(self, self._label, '')
+        default = getattr(self, self._label, None)
 
         return self.parse_attrib(
             self._label,
@@ -63,4 +116,4 @@ class EnvClass:
             default,
         )
 
-    __read_only_atributte = property(get_attribute)
+    _read_only_atributte = property(get_attribute)
